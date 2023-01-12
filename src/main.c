a875a972b707174e1bd35be4d5b102d3cfd5fcea
momeemt <momiimt@gmail.com>
2023-01-12 13:01:05 +0900

    👍 add: hit_and_miss

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 
 #define GENERATE_POINTS_COUNT 500
 #define RAND_SEED 149
+#define PI 3.14159
+#define TRIAL 10
 
 typedef struct {
     double x;
@@ -27,7 +29,7 @@ bool is_in_unit_circle (point_t *point) {
     return pow(point->x, 2) + pow(point->y, 2) <= 1;
 }
 
-void generate_rand_points (point_t *points) {
+void generate_rand_points_in_circle (point_t *points) {
     int generated_count = 0;
     while (generated_count < GENERATE_POINTS_COUNT) {
         point_t point = rand_point();
@@ -41,7 +43,7 @@ void generate_rand_points (point_t *points) {
 int make_circle_graph () {
     point_t points[GENERATE_POINTS_COUNT];
     srand(RAND_SEED);
-    generate_rand_points(points);
+    generate_rand_points_in_circle(points);
     
     FILE *data_file = fopen("data.dat", "w");
     if (data_file == NULL) {
@@ -64,6 +66,27 @@ int make_circle_graph () {
     return 0;
 }
 
+void hit_and_miss (int count) {
+    srand(RAND_SEED);
+    double average_area = 0.0;
+    for (int i = 0; i < TRIAL; i++) {
+        int hit = 0;
+        for (int j = 0; j < count; j++) {
+            point_t point = rand_point();
+            if (pow(point.x, 2) + pow(point.y, 2) <= 1.0) {
+                hit++;
+            }
+        }
+        average_area += (double)hit / (double)count / (double)TRIAL * 4.0;
+    }
+    printf("-------------点の数: %d-------------\n", count);
+    printf("面積の平均値(10試行): %f\n", average_area);
+    printf("(π-面積の平均値)/π: %f\n", (PI - average_area) / PI);
+}
+
 int main () {
-    return make_circle_graph();
+    hit_and_miss(100);
+    hit_and_miss(1000);
+    hit_and_miss(10000);
+    return 0;
 }
